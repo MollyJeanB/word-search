344b4a3579a76079b76ef544beecee63e26b8069
Molly Jean Bennett <molly.bennett@equipmentshare.com>
2022-12-30 10:40:43 -0800

    [F] WIP solution

--- src/App.js
+++ src/App.js
@@ -4,21 +4,37 @@ import { generateLetterSet } from "./generateLetterSet";
 
 function App() {
 	const [ letters, setLetters ] = useState([])
+	const [ targetWord, setTargetWord ] = useState('')
+
+	const onWordChange = (event) => {
+		const word = event.target.value
+		setTargetWord(word)
+	}
 
 	useEffect(() => {
 		setLetters( generateLetterSet() )
 	}, [])
+
+	const letterMatchesTargetWord = ( letter, index ) => {
+		for ( let i = 0; i < targetWord.length; i++ ) {
+			if ( targetWord[i] === letter ) {
+				return true
+			}
+		}
+	}
 
   return (
     <Page>
 		<h1>Word Search</h1>
 		<Input 
 			placeholder="Type a word"
+			onChange={(event) => onWordChange(event)}
 		/>
 		<PuzzleWrapper>
 			{ letters.map( (letter, index) => {
+				const letterIsHighLighted = letterMatchesTargetWord( letter, index )
 				return (
-				<PuzzleLetter key={`${letter}-${index}`}>
+				<PuzzleLetter key={`${letter}-${index}`} isHighlighted={letterIsHighLighted}>
 					{letter}
 				</PuzzleLetter>
 				)
